f85b2766c7237b2d3467076eaadef67be6385e4f
Nulldata <thebreadcat@gmail.com>
2015-01-08 15:31:43 +0100

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#![feature(macro_rules)]
+
+macro_rules! version(
+  () => (
+    format!( "{}.{}.{}", env!( "CARGO_PKG_VERSION_MAJOR" )
+                       , env!( "CARGO_PKG_VERSION_MINOR" )
+                       , env!( "CARGO_PKG_VERSION_PATCH" ) )
+  )
+);
+
+#[test]
+fn does_it_work() {
+  // Bad test is bad.
+  assert_eq!( version!(), "1.0.0" );
+}
